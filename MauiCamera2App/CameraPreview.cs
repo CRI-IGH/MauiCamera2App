61fcc23434a3f553fd87c1f044ba7a7c72cdd1a7
Christopher Grabs <Christopher.Grabs@infotec-ag.de>
2024-05-31 10:32:45 +0200

    a

--- MauiCamera2App/CameraPreview.cs
+++ MauiCamera2App/CameraPreview.cs
@@ -2,12 +2,13 @@
 using Android.Content;
 using Android.Hardware.Camera2;
 using Android.Views;
+using View = Microsoft.Maui.Controls.View;
 
 
 
 namespace MauiCamera2App
 {
-    public class CameraPreview : Android.Views.View
+    public class CameraPreview : View
     {
         public CameraPreview()
         {
